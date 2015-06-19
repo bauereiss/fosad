6a8e21e1fc3416ac2845aa2243d123054bc0d215
Thomas Bauereiss <thomas.bauereiss@dfki.de>
2015-06-19 11:34:57 +0200

    Simple_Lang: Nicer ordering of definitions

--- Simple_Lang.thy
+++ Simple_Lang.thy
@@ -8,22 +8,22 @@ datatype 'var expr =
   Var 'var
 | Plus "'var expr" "'var expr"
 
-datatype ('var, 'dom) cmd =
-  Assign 'var 'dom "'var expr" ("_ :=\<^bsub>_\<^esub> _")
-
 primrec eval :: "'var state \<Rightarrow> 'var expr \<Rightarrow> integer" where
   "eval s (Var v) = s v"
 | "eval s (Plus e1 e2) = eval s e1 + eval s e2"
 
-primrec execute :: "'var state \<Rightarrow> ('var, 'dom) cmd \<Rightarrow> 'var state" where
-  "execute s (Assign v d e) = s(v := eval s e)"
-
-primrec dom :: "('var, 'dom) cmd \<Rightarrow> 'dom" where
-  "dom (Assign v d e) = d"
-
 primrec FV :: "'var expr \<Rightarrow> 'var set" where
   "FV (Var v) = {v}"
 | "FV (Plus e1 e2) = FV e1 \<union> FV e2"
+
+datatype ('var, 'dom) cmd =
+  Assign 'var (dom: 'dom) "'var expr" ("_ :=\<^bsub>_\<^esub> _")
+
+lemma "dom (Assign v d e) = d"
+by auto
+
+primrec execute :: "'var state \<Rightarrow> ('var, 'dom) cmd \<Rightarrow> 'var state" where
+  "execute s (Assign v d e) = s(v := eval s e)"
 
 lemma coincidence:
   assumes "\<forall>v \<in> FV e. s v = t v"
